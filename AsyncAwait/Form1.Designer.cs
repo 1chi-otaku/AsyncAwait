fe1601d94e54095be4065a87dd6c77479a25e257
Serhii <mywaifuqueen@gmail.com>
2023-04-28 21:34:17 +0100

    Add project files.

---
+++ AsyncAwait/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace AsyncAwait
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView4 = new System.Windows.Forms.ListView();
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView5 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(464, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(206, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(239, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(34, 168);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(171, 83);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 81;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Phone";
+            this.columnHeader2.Width = 79;
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(294, 168);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(171, 83);
+            this.listView2.TabIndex = 3;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Name";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Phone";
+            this.columnHeader4.Width = 103;
+            // 
+            // listView3
+            // 
+            this.listView3.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listView3.HideSelection = false;
+            this.listView3.Location = new System.Drawing.Point(549, 168);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(171, 83);
+            this.listView3.TabIndex = 4;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            this.listView3.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Name";
+            this.columnHeader5.Width = 78;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Grades";
+            this.columnHeader6.Width = 101;
+            // 
+            // listView4
+            // 
+            this.listView4.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader7,
+            this.columnHeader8});
+            this.listView4.HideSelection = false;
+            this.listView4.Location = new System.Drawing.Point(162, 299);
+            this.listView4.Name = "listView4";
+            this.listView4.Size = new System.Drawing.Size(171, 83);
+            this.listView4.TabIndex = 5;
+            this.listView4.UseCompatibleStateImageBehavior = false;
+            this.listView4.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Book";
+            this.columnHeader7.Width = 74;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Expire Days";
+            this.columnHeader8.Width = 108;
+            // 
+            // listView5
+            // 
+            this.listView5.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader9});
+            this.listView5.HideSelection = false;
+            this.listView5.Location = new System.Drawing.Point(440, 299);
+            this.listView5.Name = "listView5";
+            this.listView5.Size = new System.Drawing.Size(171, 83);
+            this.listView5.TabIndex = 6;
+            this.listView5.UseCompatibleStateImageBehavior = false;
+            this.listView5.View = System.Windows.Forms.View.Details;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "6";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(372, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "7";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(627, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "8";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(239, 283);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "9";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(517, 283);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "10";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(206, 63);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            2050,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(230, 20);
+            this.numericUpDown1.TabIndex = 12;
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "Title";
+            this.columnHeader9.Width = 161;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 450);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView5);
+            this.Controls.Add(this.listView4);
+            this.Controls.Add(this.listView3);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ListView listView3;
+        private System.Windows.Forms.ListView listView4;
+        private System.Windows.Forms.ListView listView5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+    }
+}
+
---
+++ AsyncAwait/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncAwait
+{
+    public partial class Form1 : Form
+    {
+        List<Employee> employees;
+        List<Student> students;
+        List<Book> library;
+        List<Item> shop;
+        public Form1()
+        {
+            InitializeComponent();
+
+            employees = new List<Employee>
+            {
+                new Employee("Patrick", new DateTime(2001, 12, 26), "38000000021", "Kyivska 34"),
+                new Employee("Tom", new DateTime(1901, 6, 3), "38000000031", "Kyivska 28"),
+                new Employee("Oleg", new DateTime(1925, 6, 3), "38000000036", "Lvivska 3")
+            };
+            students = new List<Student>
+            {
+                new Student("Vlad","KNP-213",12,8,7,new DateTime(2023, 4, 1)),
+                new Student("Nikita","KNP-213",2,2,2,new DateTime(2023, 3, 28)),
+                new Student("Bruh","KNP-213",6,12,12,new DateTime(2023, 4, 1))
+            };
+            library = new List<Book> {
+                new Book("1234","Krown","Bruh","SEGA","2023",new DateTime(2023,4,28),new DateTime(2023,6,28)),
+                new Book("1313131","GDGDG","Bruh 2","SEGA","2018", new DateTime(2021,6,29),new DateTime(2021,7,29)),
+                new Book("13211211","GDGDG","Bruh 3","SEGA","2018", new DateTime(2018,6,29),new DateTime(2021,7,29))
+            };
+            shop = new List<Item>
+            {
+                new Item("Meat","Chicken","Ryaba",new DateTime(2023,2,4),15),
+                new Item("Meat","Beaf","Super_Beef",new DateTime(2021,2,1),12),
+                new Item("Fish","Hake","Black Sea", new DateTime(2020,2,2),12)
+            };
+        }
+        public async void Task_6(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            void Task_6Engine(){
+                foreach (var item in employees)
+                {
+                    if (item.address.Contains(textBox1.Text))
+                    {
+                        ListViewItem list_item = new ListViewItem(item.name);
+                        list_item.SubItems.Add(item.phone);
+                        listView1.Items.Add(list_item);
+                    }
+                }
+            }
+            await Task.Run(() => Task_6Engine());
+        }
+        public async void Task_7(object sender, EventArgs e)
+        {
+            listView2.Items.Clear();
+            void Task_7Engine()
+            {
+                foreach (var item in employees)
+                {
+                    if (item.date.Year == numericUpDown1.Value)
+                    {
+                        ListViewItem list_item = new ListViewItem(item.name);
+                        list_item.SubItems.Add(item.phone);
+                        listView2.Items.Add(list_item);
+                    }
+                }
+            }
+            await Task.Run(() => Task_7Engine());
+        }
+        public async void Task_8(object sender, EventArgs e)
+        {
+            listView3.Items.Clear();
+            void Task_8Engine()
+            {
+                foreach (var item in students)
+                {
+                    if (item.last_exam.Year >= numericUpDown1.Value)
+                    {
+                        ListViewItem list_item = new ListViewItem(item.name);
+                        list_item.SubItems.Add(item.math_grade.ToString() + " " + item.phys_grade.ToString() + " " + item.inform_grade.ToString());
+                        listView3.Items.Add(list_item);
+                    }
+                }
+            }
+            await Task.Run(() => Task_8Engine());
+        }
+        public async void Task_9(object sender, EventArgs e)
+        {
+            listView4.Items.Clear();
+            void Task_9Engine()
+            {
+                foreach (var item in library)
+                {
+                    if (DateTime.Now.AddDays(5) > item.time)
+                    {
+                        TimeSpan ts = DateTime.Now.Subtract(item.time);
+                        int diff = Convert.ToInt32(ts.TotalDays);
+                        ListViewItem list_item = new ListViewItem(item.title);
+                        list_item.SubItems.Add(diff.ToString());
+                        listView4.Items.Add(list_item);
+                    }
+                }
+            }
+            await Task.Run(() => Task_9Engine());
+        }
+        public async void Task_10(object sender, EventArgs e)
+        {
+            listView5.Items.Clear();
+            void Task_10Engine()
+            {
+                foreach (var item in shop)
+                {
+                    if (numericUpDown1.Value > item.time.Year)
+                    {
+                        ListViewItem list_item = new ListViewItem(item.item_name);
+                        listView5.Items.Add(list_item);
+                    }
+                }
+            }
+            await Task.Run(() => Task_10Engine());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task_6(sender,e);
+            Task_7(sender, e);
+            Task_8(sender, e);
+            Task_9(sender, e);
+            Task_10(sender, e);
+        }
+    }
+
+
+    public class Employee
+    {
+        public string name { get; set; }
+        public DateTime date { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public Employee(string name, DateTime date, string phone, string address) { 
+            this.name = name;
+            this.date = date;
+            this.phone = phone;
+            this.address = address;
+        }
+    }
+    public class Student
+    {
+        public string name { get; set; }
+        public string group { get; set; }
+        public uint math_grade { get; set; }
+        public uint phys_grade { get; set; }
+        public uint inform_grade { get; set; }
+        public DateTime last_exam { get; set; }
+        public string address { get; set; }
+        public Student(string name, string group, uint math_grade, uint phys_grade, uint inform_grade, DateTime last_exam)
+        {
+            this.name=name;
+            this.group=group;
+            this.math_grade=math_grade;
+            this.phys_grade=phys_grade;
+            this.inform_grade=inform_grade;
+            this.last_exam=last_exam;
+        }
+    }
+    public class Book
+    {
+        public string ticket_number { get; set; }
+        public string author_name { get; set; }
+        public string title { get; set; }
+        public string publisher { get; set; }
+        public string year { get; set; }
+        public DateTime get_date { get; set; }
+        public DateTime time { get; set; }
+
+        public Book(string ticket_number, string author_name, string title, string publisher, string year, DateTime get_date, DateTime time)
+        {
+            this.ticket_number = ticket_number;
+            this.author_name = author_name;
+            this.title = title;
+            this.publisher = publisher;
+            this.year = year;
+            this.get_date = get_date;
+            this.time = time;
+        }
+    }
+
+    public class Item
+    {
+        public string item_type { get; set; }
+        public string item_name { get; set; }
+        public string publisher { get; set; }
+        public DateTime time { get; set; }
+        public double best_before_days { get; set; }
+        public Item(string item_type, string item_name, string publisher, DateTime time, double best_before_days)
+        {
+            this.item_type = item_type;
+            this.item_name = item_name;
+            this.publisher = publisher;
+            this.time = time;
+            this.best_before_days = best_before_days;
+        }
+    }
+}
